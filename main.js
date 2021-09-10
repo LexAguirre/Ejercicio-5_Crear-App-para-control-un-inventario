eb9739d7c2e0a35e283660f90eb047428d348c83
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2021-09-10 18:26:24 -0500

    Started Proyect

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+import SyE from "./script.js";
+
+let serpYEscaleras = new SyE();
+console.log(serpYEscaleras.boardGame());
+
---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+export default class SyE{
+    
+    constructor(){
+        
+    }
+
+}
+
+
+
